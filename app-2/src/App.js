5775b58eb67245eb2b44558c1ac4416bf654331f
Joshua Gray <graydog.8719@gmail.com>
2024-05-13 16:13:15 -0600

    completed app-2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,23 +1,21 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+const array = ['spaghetti', 'ice cream', 'sushi', 'soup', 'bread', 'salad', 'cereal']
+
 function App() {
+  const [wordArray, setWordArray] = useState(array)
+
+  const listItems = wordArray.map((word) => 
+    <li key={word}>{word}</li>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {listItems}
+      </ul>
     </div>
   );
 }
